14e16a8fdf6b4170cb5fea3231a77f3b925743a9
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-20 11:20:10 -0700

    added socials and updated nav tabs and sections inside them

--- index.js
+++ index.js
@@ -12,29 +12,39 @@ const sectionDisplay = document.querySelector('#display')
 
 about.addEventListener('click', (e) =>{
     e.preventDefault()
-    sectionDisplay.innerHTML= `<text>I am a huge fan of making music whether it's on a computer or a guitar. 
-    Another hobby of mine is playing video games. I rather play with some friends so any game that has online
-    or couch co-op games. What I look for in life is balance and love to find humor in everything. </text> `
+    sectionDisplay.innerHTML= `<h1>About Me</h1>
+    <text>I am a huge fan of creating and playing whether its on a computer, console, or guitar. 
+    Some of the best times are spent with friends and family so I like playing some couch co-op games or watching movies with them.
+    What I look for in life is balance and love to find humor in everything. </text> `
     section.appendChild(sectionDisplay)
 })
 
 skills.addEventListener('click', (e) =>{
     e.preventDefault()
-    sectionDisplay.innerHTML= `<text> girls only want guys with good skills. </text> `
+    sectionDisplay.innerHTML= `<h1> Skills And Experience</h1>
+    <br>
+    <p>I create successful responsive websites that are easy to use. I've worked
+    with HTML, CSS, JS, building small web apps
+    <br>
+    <br>
+    I also have full-stack developer experience with express, cors, node.js</p>`
     section.appendChild(sectionDisplay)
 
 })
 
 projects.addEventListener('click', (e) =>{
     e.preventDefault()
-    sectionDisplay.innerHTML= `<text> this is where the projects appear. </text> `
+    sectionDisplay.innerHTML= `<h1>Projects</h1>
+    <br>
+    <text> this is where the projects appear. </text> `
     section.appendChild(sectionDisplay)
 
 })
 
 contact.addEventListener('click', (e) =>{
     e.preventDefault()
-    sectionDisplay.innerHTML= `<text> contact me here. </text> `
+    sectionDisplay.innerHTML= `<h1>Say Hi</h1>
+    <text> contact me here. </text> `
     section.appendChild(sectionDisplay)
 
 })
