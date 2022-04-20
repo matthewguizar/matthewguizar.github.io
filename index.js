360b4c76af808ca6b72726ff12b2914d5eb9efad
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-20 11:57:57 -0700

    ran into issues had to backtrack, but fixed

--- index.js
+++ index.js
@@ -12,39 +12,36 @@ const sectionDisplay = document.querySelector('#display')
 
 about.addEventListener('click', (e) =>{
     e.preventDefault()
-    sectionDisplay.innerHTML= `<h1>About Me</h1>
+    section.innerHTML= `<h1>About Me</h1>
     <text>I am a huge fan of creating and playing whether its on a computer, console, or guitar. 
     Some of the best times are spent with friends and family so I like playing some couch co-op games or watching movies with them.
     What I look for in life is balance and love to find humor in everything. </text> `
-    section.appendChild(sectionDisplay)
+  
 })
 
 skills.addEventListener('click', (e) =>{
     e.preventDefault()
-    sectionDisplay.innerHTML= `<h1> Skills And Experience</h1>
-    <br>
+    section.innerHTML= `<h1> Skills And Experience</h1>
     <p>I create successful responsive websites that are easy to use. I've worked
     with HTML, CSS, JS, building small web apps
-    <br>
-    <br>
     I also have full-stack developer experience with express, cors, node.js</p>`
-    section.appendChild(sectionDisplay)
+    
 
 })
 
 projects.addEventListener('click', (e) =>{
     e.preventDefault()
-    sectionDisplay.innerHTML= `<h1>Projects</h1>
+    section.innerHTML= `<h1>Projects</h1>
     <br>
     <text> this is where the projects appear. </text> `
-    section.appendChild(sectionDisplay)
+    
 
 })
 
 contact.addEventListener('click', (e) =>{
     e.preventDefault()
-    sectionDisplay.innerHTML= `<h1>Say Hi</h1>
+    section.innerHTML= `<h1>Say Hi</h1>
     <text> contact me here. </text> `
-    section.appendChild(sectionDisplay)
+    
 
 })
