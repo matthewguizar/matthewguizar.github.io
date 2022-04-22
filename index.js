2508b3aaf863194984f37d25e710192c68ba45f5
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-21 18:44:16 -0700

    finished

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const contact = document.querySelector('#contact')
 const section = document.querySelector('.section')
 const sectionDisplay = document.querySelector('#display')
 const contactBtn = document.querySelector('#home-contact-btn')
-const projectDisplay = document.querySelector('#display')
+
 
 
 const getAboutPage =  (e) =>{
@@ -33,26 +33,13 @@ const getProjects = (e) =>{
     e.preventDefault()
     section.innerHTML= `<h1>Projects</h1>
     <br>`
-    sectionDisplay.innerHTML = `<h2 id='title'> <a id='projectDisplay' href="">Where's that from</a> </h2> `
-    section.appendChild(sectionDisplay)
-
-     // section.innerHTML= `<h1>Projects</h1>
-    // <br>`
-    // sectionDisplay.innerHTML = `<h2 id='title'> Wheres that from </h2> 
-    // <p id='desc'> This is a small web app that uses movie quotes to let user guess a movie quote
-    // generate random quotes with a title, and searching a movie title to get a quote.</p>  I check them regularly. Thanks for checking out my portfolio as well. I used HTML, CSS,
-    // JavaScript, Axios, Express, and Node.js to build it. Heres a <a href="https://www.youtube.com/watch?v=warVX9BzM8k">demo</a> of the web app.`
-    // section.appendChild(sectionDisplay)
-}
-const wheresThatFrom = (e) =>{
-    e.preventDefault()
-      section.innerHTML= `<h1>Projects</h1>
-    <br>`
-    sectionDisplay.innerHTML = `<h2 id='title'> Wheres that from </h2> 
+    sectionDisplay.innerHTML = `<h3 id='title'> Wheres that from </h3> 
     <p id='desc'> This is a small web app that uses movie quotes to let user guess a movie quote
-    generate random quotes with a title, and searching a movie title to get a quote.</p>  I check them regularly. Thanks for checking out my portfolio as well. I used HTML, CSS,
-    JavaScript, Axios, Express, and Node.js to build it. Heres a <a href="https://www.youtube.com/watch?v=warVX9BzM8k" target="_blank">demo</a> of the web app. Heres the <a href="https://github.com/matthewguizar/WheresThatFrom" target="_blank">code</a> link.
-     and the<a href='https://wheresthatfrom.herokuapp.com/' target="_blank"> hosted site</a>.`
+    generate random quotes with a title, and searching a movie title to get a quote. I used HTML, CSS,
+    JavaScript, Axios, Express, and Node.js to build it. Here are some links
+    <a href="https://www.youtube.com/watch?v=warVX9BzM8k">demo</a>
+    <a href="https://github.com/matthewguizar/WheresThatFrom">code</a>
+    <a href="https://wheresthatfrom.herokuapp.com/">link</a>.</p>`
     section.appendChild(sectionDisplay)
 }
 
@@ -60,7 +47,8 @@ const contactPage = (e) =>{
     e.preventDefault()
     section.innerHTML= `<h1>Can't get enough?</h1>`
     sectionDisplay.innerHTML = `If you want to get into contact with me regarding work or just want to say what's up. Either way the best way to
-    send me an <a href="mailto:msg.gmath@gmail.com">email</a>.`
+    send me an <a href="mailto:msg.gmath@gmail.com">email</a>.
+    <img>`
     section.appendChild(sectionDisplay)
 
   
@@ -71,4 +59,3 @@ about.addEventListener('click', getAboutPage)
 skills.addEventListener('click', girlsWantSkills)
 projects.addEventListener('click', getProjects)
 contact.addEventListener('click', contactPage)
-projectDisplay.addEventListener('click', wheresThatFrom)
