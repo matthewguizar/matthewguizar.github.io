4eaca1ca0dc245f43f3c17e2cd35017d1bbc0542
Matthew Guizar <matthewrguizar@gmail.com>
2023-05-01 15:14:13 -0700

    updated

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ const contactBtn = document.querySelector('#home-contact-btn')
 const getAboutPage =  (e) =>{
     e.preventDefault()
     section.innerHTML= `<h1>About Me</h1>`
-    sectionDisplay.innerHTML= `<p> I am a huge fan of creating and playing whether its on a computer, console, or guitar. 
-    Some of the best times are spent with friends and family so I like playing some couch co-op games or watching movies with them.
-    What I look for in life is balance and love to find humor in everything.</p> `
+    sectionDisplay.innerHTML= `<p> I am currently 26 years old and enjoy learning and having new experiences. 
+    I would describe my personality as laid back, and someone who enjoys to have a good laugh. I take pride in my work
+    and love to bring others up. Some of my hobbies including playing guitar, singing, and skateboarding. I go to my hobbies
+    as a break to unwind, because I tend to push myself until I complete my goals. </p> `
     section.appendChild(sectionDisplay)
 
 }
@@ -23,28 +24,28 @@ const getAboutPage =  (e) =>{
 const girlsWantSkills = (e) =>{
     e.preventDefault()
     section.innerHTML= `<h1> Skills And Experience</h1>`
-    sectionDisplay.innerHTML = `<p> I create successful responsive websites that are easy to use. I've worked
-    with HTML, CSS, JS, building small web apps. Also have experience building apps in Java using frameworks like Spring and Hibernate</p>`
+    sectionDisplay.innerHTML = `<p> I create successful responsive websites that are easy to use. I also have experience building apps in Java using frameworks like Spring and Hibernate,
+    but have dipped my toes in other backend languages as Python and C#. I am mainly self-taught but had help with a short 
+    coding bootcamp. You may see my repositories in my github</p>`
     section.appendChild(sectionDisplay)
 }
 
 const getProjects = (e) =>{
     e.preventDefault()
-    section.innerHTML= `<h1>Projects</h1>
-    <br>`
-    sectionDisplay.innerHTML = `<h3 id='title'> Wheres that from </h3> 
-    <p id='desc'> This is a small web app that uses movie quotes to let user guess a movie quote
+    section.innerHTML= `<h1>Projects</h1>`
+    sectionDisplay.innerHTML = `<h4 id='title'> Wheres that from </h4> 
+    <p id='desc'> A web app that uses movie quotes to let user guess a movie quote
     generate random quotes with a title, and searching a movie title to get a quote. I used HTML, CSS,
     JavaScript, Axios, Express, and Node.js to build it. Here are some links
     <a href="https://www.youtube.com/watch?v=warVX9BzM8k"  target=”_blank”>demo</a>
     <a href="https://github.com/matthewguizar/WheresThatFrom"  target=”_blank”>code</a>
-    <a href="https://wheresthatfrom.herokuapp.com/"  target=”_blank”>link</a>.</p>
     
-    <h3 id ='title'> On my list</h3>
+    <h4 id ='title'> On my list</h4>
     <p id='desc'> A web app that lets users see Top Rated movies and add their to their list of favorites
     and was build using Java, Spring, Hibernate, Javascript, Bootstrap, HTML, Heroku, Postgres, and Maven.
-    <a href="https://onmylist.herokuapp.com/" target="_blank">link</a>. 
-    <a href="https://github.com/matthewguizar/watchlist" target="_blank">code</p`
+    <a href="https://github.com/matthewguizar/watchlist" target="_blank">code</a>
+    <span>These are projects I created during my bootcamp. Updated projects and what I have been learning since I graduated
+    can be found in my github profile.</span> `
     section.appendChild(sectionDisplay)
 }
 
@@ -52,7 +53,7 @@ const contactPage = (e) =>{
     e.preventDefault()
     section.innerHTML= `<h1>Just can't get enough?</h1>`
     sectionDisplay.innerHTML = `If you want to get into contact with me regarding work or just want to say what's up. Either way the best way to
-    contact me is to send me an email <a href="mailto:msg.gmath@gmail.com">here</a>. Thanks for checking out my page.
+    contact me is to send me an email <a href="mailto:matthewrguizar@gmail.com">here</a>. Thanks for checking out my page.
     <img>`
     section.appendChild(sectionDisplay)
 
